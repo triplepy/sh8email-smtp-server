5802f66fc9d425e046f1e56ed10b7e8a4417b91b
Wonyoung Ju <getogrand@gmail.com>
2017-03-09 21:51:21 +0900

    Use parser and extend the base object

--- smtp-server-config.js
+++ smtp-server-config.js
@@ -1,34 +1,31 @@
 const winston = require('winston');
+const simpleParser = require('mailparser').simpleParser;
+
+const base = {
+  name: 'sh8.email SMTP Server',
+  disabledCommands: ['AUTH'],
+  onData(stream, session, callback) {
+    let buffer = Buffer.alloc(0);
+    stream.on('data', (chunk) => {
+      buffer = Buffer.concat([buffer, chunk]);
+    });
+    stream.on('end', () => {
+      simpleParser(buffer).then((mail) => {
+        // TODO Implement rest api logic instead
+        winston.debug(mail);
+        callback();
+      }).catch((err) => {
+        callback(err);
+      });
+    });
+  },
+};
 
 module.exports = {
-  development: {
-    name: 'sh8.email SMTP Server',
+  development: Object.assign(base, {
     logger: true,
-    disabledCommands: ['AUTH'],
-    onData(stream, session, callback) {
-      let buffer = Buffer.alloc(0);
-      stream.on('data', (chunk) => {
-        buffer = Buffer.concat([buffer, chunk]);
-      });
-      stream.on('end', () => {
-        winston.info(buffer.toString());
-        callback();
-      });
-    },
-  },
-  production: {
-    name: 'sh8.email SMTP Server',
-    disabledCommands: ['AUTH'],
-    // TODO Implement production logic
-    // onData(stream, session, callback) {
-    //   let buffer = Buffer.alloc(0);
-    //   stream.on('data', (chunk) => {
-    //     buffer = Buffer.concat([buffer, chunk]);
-    //   });
-    //   stream.on('end', () => {
-    //     winston.info(buffer.toString());
-    //     callback();
-    //   });
-    // },
-  },
+  }),
+  production: Object.assign(base, {
+    // Nothing to do currently.
+  }),
 };
