f0ac63b36e4f0e61f9cf1d1c216c59490dd66508
Wonyoung Ju <getogrand@gmail.com>
2017-04-17 23:38:02 +0900

    Ship testcode and refactor testmailsender util

---
+++ test/server.js
@@ -0,0 +1,62 @@
+/* eslint-disable prefer-arrow-callback*/
+
+require('should');
+const sinon = require('sinon');
+const config = require('config');
+
+const rp = require('request-promise');
+
+require('../index');
+const server = require('../server');
+const sendmail = require('../testutils/sendmail');
+
+describe('SMTP server', function() {
+  let postStub;
+  beforeEach(function () {
+    postStub = sinon.stub(rp, 'post').callsFake(option => Promise.resolve(option.body));
+  });
+
+  it('should request to create mail to api server', function () {
+    const fromAddress = `wonyoungju@${config.host}`;
+    const toAddress = `kyunooh@${config.host}`;
+    const expected = {
+      from: [{
+        address: fromAddress,
+        name: '',
+      }],
+      to: [{
+        address: toAddress,
+        name: '',
+      }],
+      cc: [],
+      bcc: [],
+      subject: 'This is a test subject',
+      text: 'This is a test text.',
+    };
+    return sendmail({
+      from: fromAddress,
+      to: toAddress,
+      subject: 'This is a test subject',
+      text: 'This is a test text.',
+    }).then((response) => {
+      postStub.called.should.be.true();
+      postStub.callCount.should.equal(1);
+
+      const postStubArg = postStub.args[0][0];
+      const { uri, body } = postStubArg;
+      uri.should.equal(`http://${config.host}/api/mail/create`);
+      body.subject.should.equal(expected.subject);
+      body.text.trim().should.equal(expected.text);
+      body.from.should.deepEqual(expected.from);
+      body.to.should.deepEqual(expected.to);
+      body.cc.should.deepEqual(expected.cc);
+      body.bcc.should.deepEqual(expected.bcc);
+
+      return true;
+    });
+  });
+
+  after(function () {
+    server.close();
+  });
+});
---
+++ testutils/sendmail.js
@@ -0,0 +1,26 @@
+const config = require('config');
+const nodemailer = require('nodemailer');
+
+const sendmail = (data) => {
+  const transporter = nodemailer.createTransport({
+    host: config.host,
+    port: config.port,
+    ignoreTLS: true,
+  });
+  return transporter.sendMail(data); // returns promise object
+};
+
+module.exports = sendmail;
+
+if (require.main === module) {
+  sendmail({
+    from: `wonyoungju@${config.host}`,
+    to: `kyunooh@${config.host}`,
+    subject: 'This is a test subject',
+    text: 'This is a test text.',
+  }).then(() => {
+    console.info('Email was sent successfully.');
+  }).catch((err) => {
+    console.error(err);
+  });
+}
